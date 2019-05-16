dd3ec653e02fb47d6a88afca7a1d2a46e8efa061
Amirhossein <amirhossein3089@yahoo.com>
2019-05-17 01:08:59 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,187 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Markup,
+  Editor,
+  Container,
+  Column,
+  Row,
+  RuleInput,
+  RuleLabel,
+  StyleInput,
+  Button,
+  Document
+} from "./styled";
+import hljs from "highlight.js";
+import {rando,getRandomPoem} from './utils'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// console.log(rando)
+// console.log(rando.randomInt(1,10))
+
+class App extends Component {
+  state = {
+    editor: "",
+    name0: "",
+    begin0: "",
+    end0: "",
+    style0: "",
+    rules: 1
+  };
+
+  handleChange = event => {
+    let { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  get rules(){
+    let { rules } = this.state;
+    let array = [];
+    let fields = ["name", "begin", "end"];
+    for (let i = 0; i < rules; i++) {
+      array.push(
+        <Row key={i}>
+          <Column>
+            {fields.map((field, index) => {
+              return (
+                <Column key={index}>
+                  <RuleLabel>{field}</RuleLabel>
+                  <RuleInput
+                    value={this.state[`${field}${i}`]}
+                    onChange={this.handleChange}
+                    name={`${field}${i}`}
+                  />
+                </Column>
+              );
+            })}
+          </Column>
+          <StyleInput
+            value={this.state[`style${i}`]}
+            onChange={this.handleChange}
+            name={`style${i}`}
+          />
+        </Row>
+      );
+    }
+
+    return array;
+  };
+
+  prepareStyles = () => {
+    let {rules} = this.state
+    let styles = []
+    for (let i = 0; i < rules; i++) {
+      styles.push(`
+        .hljs-${this.state['name' + i]} {
+          ${this.state['style' + i]}
+        }
+      `)
+    }
+
+    let newStyles = "".concat(styles).replace(",", "")
+
+    while (newStyles.includes('random')) {
+      newStyles = newStyles.replace('random', rando.color())
+    }
+
+    return newStyles
+  }
+
+
+  newFields = () => {
+    this.setState(prevState => {
+      let { rules } = prevState;
+      let fields = ["name", "begin", "end", "style"];
+      let inputValues = {};
+      fields.forEach(field => {
+        inputValues = {
+          ...inputValues,
+          [`${field}${rules}`]: ""
+        };
+      });
+      rules++;
+      console.log({ rules, ...inputValues });
+      return {
+        rules,
+        ...inputValues
+      };
+    });
+  };
+
+  convertToMarkup = (text = "") => {
+    return {
+      __html: hljs.highlightAuto(text).value
+    };
+  };
+
+  language = newRules => {
+    return () => ({
+      contains: [...newRules]
+    });
+  };
+  registerLanguage = state => {
+    let { rules } = state;
+    let ruleObjects = [];
+    for (let i = 0; i < rules; i++) {
+      let newRule = {
+        className: state[`name${i}`],
+        begin: state[`begin${i}`],
+        end: state[`end${i}`]
+      };
+      let { className, begin, end } = newRule;
+      if (className.length > 1 && begin.length > 1 && end.length > 1) {
+        begin = new RegExp(begin);
+        end = new RegExp(end);
+        ruleObjects.push(newRule);
+      }
+    }
+    hljs.registerLanguage("language", this.language(ruleObjects));
+    hljs.configure({
+      languages: ["language"]
+    });
+  };
+
+  componentWillUpdate(nextProps, nextState) {
+    this.registerLanguage(nextState);
+  }
+
+
+  getRandomText = async() =>  {
+    try{
+      let poem = await getRandomPoem()
+      this.handleChange({
+        target : {
+          name:'editor',
+          value:poem 
+        }
+      })
+    }
+    catch(error){
+      console.log("ghergher")
+    }
+  }
+
+  render() {
+    let { editor } = this.state;
+    let { handleChange, newFields, rules ,prepareStyles,convertToMarkup,getRandomText} = this;
+    return (
+      <Container>
+        <Column>
+          {rules}
+          <Button onClick={newFields}>New Rule</Button>
+        </Column>
+
+        <Column>
+          <Button onClick={getRandomText}>Random Text</Button>
+          <Document>
+            <Editor name={"editor"} value={editor} onChange={handleChange} />
+            <Markup customStyles={prepareStyles()}
+              dangerouslySetInnerHTML={convertToMarkup(editor)} />
+          </Document>
+        </Column>
+      </Container>
+    );
+  }
 }
 
 export default App;
